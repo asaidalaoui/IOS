e277ac910de4adfb4954aba0a4746448a4f1df5d
Abderrahman SaidAlaoui <a.saidalaoui@utexas.edu>
2017-03-15 10:55:33 -0500

    modified LoginViewController.swift, needs login password check against password saved in coredata

--- IOSProject/LoginViewController.swift
+++ IOSProject/LoginViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class LoginViewController: UIPageViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userNameTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    
+    var alertController: UIAlertController? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.userNameTxtFld.delegate = self
+        self.passwordTxtFld.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +28,52 @@ class LoginViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    //performs checks prior to segue to the next view.
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
+        if let ident = identifier {
+            if ident == "loginSeg" {
+                if let name = self.userNameTxtFld.text, let pw = self.passwordTxtFld.text {
+                    if name == "" || pw == "" {
+                        showAlert(errorMsg: "missing username or password")
+                        return false
+                    } else if !checkCredentials() {
+                        showAlert(errorMsg: "Wrong username or password")
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+    
+    //check entered password in passwordTxtFld against value registered in coredata for given username
+    //return false if password does not match stored value.
+    func checkCredentials () -> Bool {
+        //TODO: implement the logic for this function.
+        //return true to access user's main view
+        return false
+    }
+    
+    func showAlert(errorMsg: String) {
+        self.alertController = UIAlertController(title: "Error", message: "\(errorMsg)", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+        self.alertController!.addAction(OKAction)
+        
+        self.present(self.alertController!, animated: true, completion:nil)
+    }
+    
+    //dismissing the keyboard when pressing anywhere else on the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameTxtFld.resignFirstResponder()
+        passwordTxtFld.resignFirstResponder()
+        return true
+    }
+    
     /*
     // MARK: - Navigation
 
