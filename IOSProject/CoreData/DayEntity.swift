ca3d0079e6d1a88f1d3934d08c63945898bf6124
Ali Darwiche <Rabab@Ali-Darwiches-MacBook-Pro.local>
2017-03-18 16:17:01 -0500

    altered DayEntity

--- IOSProject/CoreData/DayEntity.swift
+++ IOSProject/CoreData/DayEntity.swift
@@ -14,11 +14,35 @@ class DayEntity {
     private var managedContext: NSManagedObjectContext!
     private var curUser:User!
     private var days:[Day]!
+    private var day:Day!
     
-    init(){
+    init(day:String){
         curUser = UserEntity().get(name: UserDefaults.standard.object(forKey: "curUser") as! String)
         let sortDesc = NSSortDescriptor(key: "priority", ascending: true)
         days = curUser.days?.sortedArray(using: [sortDesc]) as! [Day]
+        
+        var index:Int = 0
+        switch(day) {
+        case "Monday": index = 0
+            break
+        case "Tuesday": index = 1
+            break
+        case "Wednesday": index = 2
+            break
+        case "Thursday": index = 3
+            break
+        case "Friday": index = 4
+            break
+        case "Saturday": index = 5
+            break
+        case "Sunday": index = 7
+            break
+        default: index = -1
+            break
+        }
+        self.day = days[index]
+
+        
     }
     
     func access() {
@@ -26,15 +50,13 @@ class DayEntity {
         managedContext = appDelegate.persistentContainer.viewContext
     }
     
-    func changeSleep(dayOfWeek:String, sleepHours:Double) -> Bool {
+    func changeSleep(sleepHours:Double) -> Bool {
         access()
-        let index = getDayIndex(dayOfWeek: dayOfWeek)
-        let day = days[index]
         day.sleepHours = sleepHours
         
         //Store change
-        days[index] = day
-        curUser.days = NSSet(array: days)
+//        days[index] = day
+//        curUser.days = NSSet(array: days)
         do {
             try managedContext.save()
             return true
@@ -47,15 +69,15 @@ class DayEntity {
         return false
     }
     
-    func changeBusy(dayOfWeek:String, busyHours:Double) -> Bool {
+    func changeBusy(busyHours:Double) -> Bool {
         access()
-        let index = getDayIndex(dayOfWeek: dayOfWeek)
-        let day = days[index]
+//        let index = getDayIndex(dayOfWeek: dayOfWeek)
+//        let day = days[index]
         day.busyHours = busyHours
         
         //Store change
-        days[index] = day
-        curUser.days = NSSet(array: days)
+//        days[index] = day
+//        curUser.days = NSSet(array: days)
         do {
             try managedContext.save()
             return true
