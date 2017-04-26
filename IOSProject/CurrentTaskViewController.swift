a3d1280d1c621ed116fc7a2e478c0267c118ffca
Abderrahman SaidAlaoui <a.saidalaoui@utexas.edu>
2017-04-26 15:27:28 -0500

    update current view controller

--- IOSProject/CurrentTaskViewController.swift
+++ IOSProject/CurrentTaskViewController.swift
@@ -100,14 +100,19 @@ class CurrentTaskViewController: UIViewController {
         
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "createTask") {
+            let destinationVC = segue.destination as? AddTaskViewController
+            destinationVC?.segControlIndex = 0
+        }
     }
-    */
+    
 
 }
